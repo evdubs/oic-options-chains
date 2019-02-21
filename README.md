54f4762687d445068c5a53968014ec3e55ad1904
evdubs <evdubs@gmail.com>
2019-02-20 15:40:18 -1000

    add dat links

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ $ racket transform-load.rkt
 ```
 
 The provided schema.sql file shows the expected schema within the target PostgreSQL instance. This process assumes you can write to a /var/tmp/oic/options-chains folder. This process also assumes you have loaded your database with the NASDAQ symbol file information and SPDR ETF Holding information. This data is provided by the [nasdaq-symbols](https://github.com/evdubs/nasdaq-symbols) project and [spdr-etf-holdings](https://github.com/evdubs/spdr-etf-holdings) project. These programs currently (2019-02-06) will just extract option chains for S&P 500 component companies and some SPDR ETFs. The transform/load script will just insert options expiring 2 weeks, 4 weeks, and 8 weeks from the current date and they will also just grab the +/- 0%, 2%, 4% and 8% strikes. This suits my purposes, but this filter can be removed if you want to insert everything.
+
+## dat
+
+Historical OIC options chains can be retrieved in CSV form from [dat](https://github.com/datproject/dat) with the following URL:
+
+`dat://8eed8dc41ab7ff1ac84ce4c4cf364bf04218b719b601c802ca6bcad46421b748`
+
+You may also view this link on [datbase](https://datbase.org/view?query=dat://8eed8dc41ab7ff1ac84ce4c4cf364bf04218b719b601c802ca6bcad46421b748).
---
+++ dump-dat.rkt
@@ -0,0 +1,95 @@
+#lang racket/base
+
+(require db
+         racket/cmdline
+         racket/vector
+         srfi/19) ; Time Data Types and Procedures
+
+(define start-date (make-parameter (date->string (current-date) "~1")))
+
+(define end-date (make-parameter (date->string (current-date) "~1")))
+
+(define db-user (make-parameter "user"))
+
+(define db-name (make-parameter "local"))
+
+(define db-pass (make-parameter ""))
+
+(command-line
+ #:program "racket dump-dat.rkt"
+ #:once-each
+ [("-e" "--end-date") end
+                      "Final date for history retrieval. Defaults to today"
+                      (end-date end)]
+ [("-n" "--db-name") name
+                     "Database name. Defaults to 'local'"
+                     (db-name name)]
+ [("-p" "--db-pass") password
+                     "Database password"
+                     (db-pass password)]
+ [("-s" "--start-date") start
+                        "Earliest date for history retrieval. Defaults to today"
+                        (start-date start)]
+ [("-u" "--db-user") user
+                     "Database user name. Defaults to 'user'"
+                     (db-user user)])
+
+(define dbc (postgresql-connect #:user (db-user) #:database (db-name) #:password (db-pass)))
+
+(define (vector->csv-line vec)
+  (if (= 1 (vector-length vec))
+      (vector-ref vec 0)
+      (string-append (vector-ref vec 0) "," (vector->csv-line (vector-drop vec 1)))))
+
+(for-each (λ (date)
+            (call-with-output-file (string-append "/var/tmp/dat/oic/option-chain/" date ".csv")
+              (λ (out)
+                (displayln "act_symbol,expiration,strike,call_put,date,bid,ask,vol,delta,gamma,theta,vega,rho" out)
+                (for-each (λ (row)
+                            (displayln (vector->csv-line row) out))
+                          (query-rows dbc "
+select
+  act_symbol::text,
+  expiration::text,
+  strike::text,
+  call_put::text,
+  date::text,
+  bid::text,
+  ask::text,
+  vol::text,
+  delta::text,
+  gamma::text,
+  theta::text,
+  vega::text,
+  rho::text
+from
+  oic.option_chain
+where
+  date = $1::text::date and
+  bid is not null and
+  ask is not null and
+  vol is not null and
+  delta is not null and
+  gamma is not null and
+  theta is not null and
+  vega is not null and
+  rho is not null
+order by
+  act_symbol, expiration, strike, call_put, date;
+"
+                                      date)))
+              #:exists 'replace))
+          (query-list dbc "
+select distinct
+  date::text
+from
+  oic.option_chain
+where
+  date >= $1::text::date and
+  date <= $2::text::date
+order by
+  date;
+"
+                      (start-date)
+                      (end-date)))
+
