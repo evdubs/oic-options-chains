a9b28ef1aa21e7382b99380e394863b48db89fe7
evdubs <1124030+evdubs@users.noreply.github.com>
2025-04-25 10:11:24 -1000

    save a few more strikes so that we can still construct some strategies when vol is high

--- transform-load.2024-04-26.rkt
+++ transform-load.2024-04-26.rkt
@@ -110,11 +110,13 @@ where
   act_symbol = $2;"
                                   (date->iso8601 (folder-date))
                                   ticker-symbol)]
-         [target-strikes (list (* mark-price 80/100) (* mark-price 825/1000) (* mark-price 85/100) (* mark-price 875/1000)
+         [target-strikes (list (* mark-price 70/100) (* mark-price 75/100)
+                               (* mark-price 80/100) (* mark-price 825/1000) (* mark-price 85/100) (* mark-price 875/1000)
                                (* mark-price 90/100) (* mark-price 92/100) (* mark-price 94/100) (* mark-price 96/100) (* mark-price 98/100)
                                mark-price (* mark-price 102/100) (* mark-price 104/100) (* mark-price 106/100) (* mark-price 108/100)
                                (* mark-price 110/100) (* mark-price 1125/1000) (* mark-price 115/100) (* mark-price 1175/1000)
-                               (* mark-price 120/100))]
+                               (* mark-price 120/100) (* mark-price 125/100)
+                               (* mark-price 130/100))]
          [target-expirations (list (+days (folder-date) (* 7 2))
                                    (+days (folder-date) (* 7 4))
                                    (+days (folder-date) (* 7 8)))]
