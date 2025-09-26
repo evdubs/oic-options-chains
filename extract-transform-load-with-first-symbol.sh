ee0ab91667fe82fa2553d2d2eb24da07fb3d9b53
evdubs <1124030+evdubs@users.noreply.github.com>
2025-09-26 07:40:45 -1000

    add script to do the full etl by picking up where an interruption occurred

---
+++ extract-transform-load-with-first-symbol.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+today=$(date "+%F")
+dir=$(dirname "$0")
+
+racket -y ${dir}/extract.2023-11-16.rkt -p "$1" -e "$2" -s "$3" -f "$4"
+racket -y ${dir}/transform-load.2025-08-19.rkt -p "$1"
+
+7zr a /var/local/oic/options-chains/${today}.7z /var/local/oic/options-chains/${today}/*.html /var/local/oic/options-chains/${today}/*.json
+
+racket -y ${dir}/dump-dolt.rkt -p "$1"
